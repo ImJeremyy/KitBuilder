62e809bf2e1dfa07e952b867cee6348d2bc54155
Jeremy <madivoso@gmail.com>
2019-04-13 12:05:15 -0400

    Added main class
    - Extends from JavaPlugin
    - Added onEnable() and onDisable() methods (overrides)

---
+++ src/me/markiscool/kitbuilder/KitBuilderPlugin.java
@@ -0,0 +1,17 @@
+package me.markiscool.kitbuilder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KitBuilderPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
