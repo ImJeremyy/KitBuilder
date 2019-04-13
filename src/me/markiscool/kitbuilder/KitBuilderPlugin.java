d13d4ede32e203ac7bf621e2596aaa1bd406d786
Jeremy <madivoso@gmail.com>
2019-04-13 12:23:35 -0400

    Implemented KitManager instance in Plugin class
    - added KitManager instance
    - Added regsiterManagers() for instantiating manager classes
    - added getKitManager() - returns an instance of KitManager.

--- src/me/markiscool/kitbuilder/KitBuilderPlugin.java
+++ src/me/markiscool/kitbuilder/KitBuilderPlugin.java
@@ -1,17 +1,28 @@
 package me.markiscool.kitbuilder;
 
+import me.markiscool.kitbuilder.kit.KitManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KitBuilderPlugin extends JavaPlugin {
 
+    private KitManager kitManager;
+
     @Override
     public void onEnable() {
-
+        registerManagers();
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    private void registerManagers() {
+        kitManager = new KitManager(this);
+    }
+
+    public KitManager getKitManager() {
+        return kitManager;
     }
 
 }
