ff75571b91aeabb85906b7305c0bf6b27ec060e1
Jeremy <madivoso@gmail.com>
2019-04-13 15:31:31 -0400

    Update KitBuilderPlugin.java
    Added a lot of comments
    - new getDelay() method - get scheduler delay (in ticks) based off of config.yml debug-mode state (boolean)
    - new registerListeners() - meant to only be used in onEnable()
    - now initializes debug-mode boolean in config.yml if does not exist.

--- src/me/markiscool/kitbuilder/KitBuilderPlugin.java
+++ src/me/markiscool/kitbuilder/KitBuilderPlugin.java
@@ -1,28 +1,71 @@
 package me.markiscool.kitbuilder;
 
 import me.markiscool.kitbuilder.kit.KitManager;
+import me.markiscool.kitbuilder.listeners.GUIClickListener;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KitBuilderPlugin extends JavaPlugin {
 
     private KitManager kitManager;
 
+    /**
+     * Run when the server turns on
+     */
     @Override
     public void onEnable() {
         registerManagers();
     }
 
+    /**
+     * Run when server is shutting down
+     */
     @Override
     public void onDisable() {
 
     }
 
+    /**
+     * Registeres manager classes as well as the main config (config.yml).
+     */
     private void registerManagers() {
         kitManager = new KitManager(this);
+        if(!getConfig().contains("debug-mode")) {
+            getConfig().set("debug-mode", false);
+            saveConfig();
+        }
     }
 
+    /**
+     * Regsiters listeners
+     * See me.markiscool.kitbuilder.listeners package for all the listeners
+     */
+    private void registerListeners() {
+        Object[] listeners = {
+                new GUIClickListener(this)
+        };
+        PluginManager pm = getServer().getPluginManager();
+        for(Object o : listeners) {
+            pm.registerEvents((Listener) o, this);
+        }
+    }
+
+    /**
+     * Get the instance of KitManager
+     * @return KitManager instance
+     */
     public KitManager getKitManager() {
         return kitManager;
+    }
+
+    /**
+     * Returns 20 (1 minute). if debug mode is true. Otherwise, returns 180000 (15 minutes)
+     * 20 ticks = 1 second.
+     * @return Push scheduler delay in ticks
+     */
+    public int getDelay() {
+        return getConfig().getBoolean("debug-mode") ? 20 : 18000;
     }
 
 }
