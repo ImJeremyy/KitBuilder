992809b8a7f9a01213a86bb47e5aa3c50fe22f8b
Jeremy <madivoso@gmail.com>
2019-04-14 16:58:39 -0400

    Kit object fully functional
    - New method: generateKitGUI() - Generates the KitGUI for editing
    - Implemented functionality to the constructor for pulling - receives the ConfigurationSection object and makes the kit based off of that.
    - New method: setItems() - set the Kit items (Map<Integer, ItemStack> Integer - slot, ItemStack - the item)
    - Added more comments and such

--- src/me/markiscool/kitbuilder/kit/Kit.java
+++ src/me/markiscool/kitbuilder/kit/Kit.java
@@ -2,13 +2,17 @@ package me.markiscool.kitbuilder.kit;
 
 import me.markiscool.kitbuilder.utility.Items;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,8 +34,9 @@ public class Kit {
         this.name = name;
         this.permission = new Permission(permissionBeginning + name.toLowerCase());
         this.permission.setDefault(PermissionDefault.OP);
-        this.items = new HashMap<Integer, ItemStack>();
+        this.items = new HashMap<>();
         this.generateGUI();
+        this.generateKitGUI();
     }
 
     /**
@@ -43,7 +48,41 @@ public class Kit {
         this.name = name;
         this.permission = new Permission(permissionBeginning + name.toLowerCase());
         this.permission.setDefault(PermissionDefault.OP);
+        this.items = new HashMap<>();
         this.generateGUI();
+
+        if(kitItems != null) {
+            for(String i : kitItems.getKeys(false)) {
+                //eliminates the _# at the end of each material name and gets the material name in the end
+                char[] m = i.toCharArray();
+                char[] f = new char[m.length - 2];
+                for(int l = 0; l < f.length; l++) {
+                    f[l] = m[l];
+                }
+                String matName = new String(f).toLowerCase();
+                Material material = Material.valueOf(matName.toUpperCase());
+                int amount = kitItems.getInt(i + ".amount");
+                ItemStack item = new ItemStack(material, amount);
+                ItemMeta meta = item.getItemMeta();
+                if(kitItems.contains(i + ".meta")) {
+                    meta.setDisplayName(kitItems.getString(i + ".meta.display_name"));
+                    if(kitItems.contains(i + ".meta.lore")) {
+                        meta.setLore(new ArrayList<>(kitItems.getStringList(i + ".meta.lore")));
+                    }
+                }
+                if(kitItems.contains(i + ".enchantments")) {
+                    for(String ench : kitItems.getConfigurationSection(i + ".enchantments").getKeys(false)) {
+                        Enchantment enchantment = Enchantment.getByName(ench);
+                        int level = kitItems.getInt(i + ".enchantments." + ench);
+                        meta.addEnchant(enchantment, level, true);
+                    }
+                }
+                item.setItemMeta(meta);
+                int slot = kitItems.getInt(i + ".slot");
+                items.put(slot, item);
+            }
+        }
+        this.generateKitGUI();
     }
 
     /**
@@ -56,7 +95,23 @@ public class Kit {
         }
         gui.setItem(10, Items.kit);
         gui.setItem(13, Items.receive);
-        gui.setItem(17, Items.delete);
+        gui.setItem(16, Items.delete);
+    }
+
+    /**
+     * Generates the Kit GUI - only meant to be used in constructor
+     */
+    public void generateKitGUI() {
+        kitgui = Bukkit.createInventory(null, 54, name);
+        for(Map.Entry<Integer, ItemStack> entry : items.entrySet()) {
+            ItemStack item = entry.getValue();
+            int slot = entry.getKey();
+            kitgui.setItem(slot, item);
+        }
+        for(int i = 36; i < 45; i++) {
+            kitgui.setItem(i, Items.black);
+        }
+        kitgui.setItem(53, Items.save);
     }
 
     /**
@@ -76,10 +131,20 @@ public class Kit {
     }
 
     /**
-     * @return Array of ItemStack objects that the kit contains
+     * Integer is the inventory slot number
+     * ItemStack is the ItemStack/
+     * @return Map of Integer-ItemStack
      */
     public Map<Integer, ItemStack> getItems() {
         return items;
+    }
+
+    /**
+     *
+     * @param items
+     */
+    public void setItems(Map<Integer, ItemStack> items) {
+        this.items = items;
     }
 
     /**
