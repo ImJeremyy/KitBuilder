b66a890df3bc5f777ec89b604d20abaa2d19c1c0
Jeremy <madivoso@gmail.com>
2019-04-14 16:55:22 -0400

    KitsCommand fully functional
    - Anticipates for Permission
    - Simply lists all the available kits.
    - TODO: Make /kits visibility to only the kits they have permission to

--- src/me/markiscool/kitbuilder/commands/KitsCommand.java
+++ src/me/markiscool/kitbuilder/commands/KitsCommand.java
@@ -1,18 +1,42 @@
 package me.markiscool.kitbuilder.commands;
 
 import me.markiscool.kitbuilder.KitBuilderPlugin;
+import me.markiscool.kitbuilder.kit.Kit;
+import me.markiscool.kitbuilder.kit.KitManager;
+import me.markiscool.kitbuilder.utility.Chat;
+import me.markiscool.kitbuilder.utility.Lang;
+import me.markiscool.kitbuilder.utility.Perm;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.Set;
+
 public class KitsCommand implements CommandExecutor {
 
-    public KitsCommand(KitBuilderPlugin plugin) {
+    private String prefix;
+    private KitManager m_kit;
 
+    public KitsCommand(KitBuilderPlugin plugin) {
+        m_kit = plugin.getKitManager();
+        prefix = Lang.PREFIX.getMessage();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        return false;
+        if(sender.hasPermission(Perm.KITS.getPermission())) {
+            Set<Kit> kits = m_kit.getKits();
+            if(!kits.isEmpty()) {
+                String msg = ChatColor.GOLD + "Kits: "+ ChatColor.WHITE;
+                for(Kit kit : kits) {
+                    msg += kit.getName()  + ", ";
+                }
+                sender.sendMessage(msg);
+            } else {
+                sender.sendMessage(prefix + Chat.colourize("&cNo kits! &7Create one with /createkit <kit_name>"));
+            }
+        }
+        return true;
     }
 }
