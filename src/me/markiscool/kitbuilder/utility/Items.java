3e2b1003870a7d740864a7f88d0470227e074902
Jeremy <madivoso@gmail.com>
2019-04-14 13:44:58 -0400

    New black and save ItemStack
    These ItemStacks are for the kit gui

--- src/me/markiscool/kitbuilder/utility/Items.java
+++ src/me/markiscool/kitbuilder/utility/Items.java
@@ -13,12 +13,16 @@ public class Items {
     public static ItemStack kit;
     public static ItemStack delete;
     public static ItemStack receive;
+    public static ItemStack black;
+    public static ItemStack save;
 
     static {
         blank = generateItemStack(Material.WHITE_STAINED_GLASS_PANE, "", Arrays.asList("&f-"));
         kit = generateItemStack(Material.DIAMOND_SWORD, "&bEdit this kit", Arrays.asList("&aAdd and remove items in the kit"));
         delete = generateItemStack(Material.BARRIER, "&cDelete this kit", Arrays.asList("&cYOU CANNOT UNDO THIS!"));
         receive = generateItemStack(Material.EMERALD, "&aReceive this kit", Arrays.asList("&bMake sure you have room in your inventory!"));
+        black = generateItemStack(Material.BLACK_STAINED_GLASS_PANE, "", Arrays.asList("&l-x-"));
+        save = generateItemStack(Material.EMERALD_BLOCK,"&aSave Changes", Arrays.asList("&aClick this will save the kit changes above."));
     }
 
     /**
